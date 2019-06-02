35714252c0975ab8ba91755f9081d72c87e76b6d
Hereislittlemushroom <hereislittlemushroom@gmail.com>
2019-06-03 02:26:00 +0800

    dw

--- test_js_macOS.py
+++ test_js_macOS.py
@@ -2,22 +2,25 @@
 # @Time    : 2019/1/28 14:23
 # @Author  : fangzeqiang_cover
 # @File    : test_js.py
+# pyechartsV0.5 pip3 install pyecharts==0.5.11
 
-#V0.5 pip3 install pyecharts==0.5.11
 from PyQt5.QtCore import QUrl, Qt
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QDialog, QApplication, QHBoxLayout, QWidget, QGridLayout, QLabel, QSpinBox, \
-    QSpacerItem, QSizePolicy, QComboBox
+    QSpacerItem, QSizePolicy, QComboBox, QLineEdit
 from random import randint
 from pyecharts import Bar, Pie, Line, Overlap
 from pyecharts_javascripthon.api import TRANSLATOR
 
-TITLE_TEXT = "图表大标题"
-TITLE_SUBTEXT = "副标题"
+#TITLE_TEXT = "图表大标题"
+#TITLE_SUBTEXT = "副标题"
 ATTR = ["属性1", "属性2", "属性3", "属性4", "属性5", "属性6"]
 
 
 class Form(QDialog):
+
+    TITLE_TEXT = "这是大标题"
+    TITLE_SUBTEXT = "这是副标题"
     def __init__(self):
         super(Form, self).__init__()
 
@@ -26,11 +29,14 @@ class Form(QDialog):
         self.initUi()
         self.load_url()
 
+    # 初始化UI界面
     def initUi(self):
         self.hl = QHBoxLayout(self)
         self.widget = QWidget()
         self.gl = QGridLayout(self.widget)
+
         # ATTR1
+        #第一个输入属性值的文本框
         label1 = QLabel(ATTR[0] + ':')
         self.gl.addWidget(label1, 1 - 1, 0, 1, 1)
         self.spinbox1 = QSpinBox()
@@ -39,7 +45,7 @@ class Form(QDialog):
         self.spinbox1.valueChanged.connect(self.set_options)
         self.spinbox1.setMaximum(1000)
         self.spinbox1.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox1, 1 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox1, 1 - 1, 1, 1, 2)
         # ATTR2
         label2 = QLabel(ATTR[1] + ':')
         self.gl.addWidget(label2, 2 - 1, 0, 1, 1)
@@ -49,7 +55,7 @@ class Form(QDialog):
         self.spinbox2.valueChanged.connect(self.set_options)
         self.spinbox2.setMaximum(1000)
         self.spinbox2.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox2, 2 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox2, 2 - 1, 1, 1, 2)
         # ATTR3
         label3 = QLabel(ATTR[2] + ':')
         self.gl.addWidget(label3, 3 - 1, 0, 1, 1)
@@ -59,7 +65,7 @@ class Form(QDialog):
         self.spinbox3.valueChanged.connect(self.set_options)
         self.spinbox3.setMaximum(1000)
         self.spinbox3.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox3, 3 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox3, 3 - 1, 1, 1, 2)
         # ATTR4
         label4 = QLabel(ATTR[3] + ':')
         self.gl.addWidget(label4, 4 - 1, 0, 1, 1)
@@ -69,7 +75,7 @@ class Form(QDialog):
         self.spinbox4.valueChanged.connect(self.set_options)
         self.spinbox4.setMaximum(1000)
         self.spinbox4.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox4, 4 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox4, 4 - 1, 1, 1, 2)
         # ATTR5
         label5 = QLabel(ATTR[4] + ':')
         self.gl.addWidget(label5, 5 - 1, 0, 1, 1)
@@ -79,7 +85,7 @@ class Form(QDialog):
         self.spinbox5.valueChanged.connect(self.set_options)
         self.spinbox5.setMaximum(1000)
         self.spinbox5.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox5, 5 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox5, 5 - 1, 1, 1, 2)
         # ATTR6
         label6 = QLabel(ATTR[5] + ':')
         self.gl.addWidget(label6, 6 - 1, 0, 1, 1)
@@ -89,19 +95,50 @@ class Form(QDialog):
         self.spinbox6.valueChanged.connect(self.set_options)
         self.spinbox6.setMaximum(1000)
         self.spinbox6.setValue(randint(0, 1000))
-        self.gl.addWidget(self.spinbox6, 6 - 1, 1, 1, 1)
+        self.gl.addWidget(self.spinbox6, 6 - 1, 1, 1, 2)
+        #属性文本框设置结束
 
+        #左侧菜单设置
         self.hl.addWidget(self.widget)
         vs = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        self.gl.addItem(vs, 6, 0, 1, 2)
+        self.gl.addItem(vs, 8, 0, 1, 2)
+        
+        #输入大标题与副标题
+        #大标题
+        label_title = QLabel( '大标题'+ ':')
+        self.gl.addWidget(label_title, 6, 0, 1, 2) #addWidget里的四个数字什么意思
+        self.QLineEdit1 = QLineEdit()        #查询pyqt的文本输入框是什么控件
+        #self.QLineEdit1.setText(self.TITLE_TEXT)
+        #self.TITLE_TEXT=self.QLineEdit1.getTextMargins()
+        self.QLineEdit1.setPlaceholderText(self.TITLE_TEXT)
+        self.QLineEdit1.cursorMoveStyle='VisualMoveStyle'
+        self.QLineEdit1.returnPressed.connect(self.reload_canvas)
+        self.gl.addWidget(self.QLineEdit1, 6, 1, 1, 2)
+        #小标题
+        label_subtitle = QLabel( '小标题'+ ':')
+        self.gl.addWidget(label_subtitle, 7, 0, 1, 2)
+        self.QLineEdit2 = QLineEdit()        #查询pyqt的文本输入框是什么控件
+        self.QLineEdit2.setPlaceholderText(self.TITLE_TEXT)
+        self.QLineEdit2.cursorMoveStyle='VisualMoveStyle'
+        self.QLineEdit2.returnPressed.connect(self.reload_canvas)
+        self.gl.addWidget(self.QLineEdit2, 7, 1, 1, 2)
+        #输入大标题与副标题
+
+        #图例种类选择
+        label_kind = QLabel( '图例'+ ':')
+        self.gl.addWidget(label_kind, 9, 0, 1, 2)
         self.combobox_type = QComboBox()
         self.combobox_type.currentIndexChanged.connect(self.reload_canvas)
         self.combobox_type.addItems(['饼图', '柱状图', '折线图', '折线\柱状图'])
-        self.gl.addWidget(self.combobox_type, 7, 0, 1, 2)
+        self.gl.addWidget(self.combobox_type, 9, 1, 1, 2)
+        
+        #主题选择按钮
+        label_theme = QLabel('主题' + ':')
+        self.gl.addWidget(label_theme, 10, 0, 1, 2)
         self.combobox_theme = QComboBox()
         self.combobox_theme.currentTextChanged.connect(self.change_theme)
-        self.combobox_theme.addItems(['日间模式', '暗黑模式'])
-        self.gl.addWidget(self.combobox_theme, 8, 0, 1, 2)
+        self.combobox_theme.addItems(['light', 'dark'])
+        self.gl.addWidget(self.combobox_theme, 10, 1, 1, 2)
         # 添加web view
         self.view = QWebEngineView()
         self.view.setContextMenuPolicy(Qt.NoContextMenu)
@@ -129,6 +166,8 @@ class Form(QDialog):
         self.view.loadFinished.connect(self.set_options)
 
     def reload_canvas(self):
+        self.TITLE_TEXT=self.QLineEdit1.displayText()
+        self.TITLE_SUBTEXT=self.QLineEdit2.displayText()
         if not self.view:
             return
             # 重载画布
@@ -187,14 +226,14 @@ class Form(QDialog):
         return options
 
     def create_pie(self, v):
-        pie = Pie(TITLE_TEXT, TITLE_SUBTEXT)
+        pie = Pie(self.TITLE_TEXT, self.TITLE_SUBTEXT)
         pie.add("属性", ATTR, v, is_label_show=True)
         snippet = TRANSLATOR.translate(pie.options)
         options = snippet.as_snippet()
         return options
 
     def create_bar(self, v):
-        bar = Bar(TITLE_TEXT, TITLE_SUBTEXT)
+        bar = Bar(self.TITLE_TEXT, self.TITLE_SUBTEXT)
         bar.add('属性1', ATTR, v, is_more_utils=True)
         bar.add('属性2', ATTR, v, is_more_utils=True)
         snippet = TRANSLATOR.translate(bar.options)
@@ -202,16 +241,16 @@ class Form(QDialog):
         return options
 
     def create_line(self, v):
-        line = Line(TITLE_TEXT, TITLE_SUBTEXT)
+        line = Line(self.TITLE_TEXT, self.TITLE_SUBTEXT)
         line.add("属性", ATTR, v, is_smooth=True, mark_line=["max", "average"])
         snippet = TRANSLATOR.translate(line.options)
         options = snippet.as_snippet()
         return options
 
     def create_line_bar(self, v):
-        line = Line(TITLE_TEXT, TITLE_SUBTEXT)
+        line = Line(self.TITLE_TEXT, self.TITLE_SUBTEXT)
         line.add("属性", ATTR, v, is_smooth=True, mark_line=["max", "average"])
-        bar = Bar(TITLE_TEXT, TITLE_SUBTEXT)
+        bar = Bar(self.TITLE_TEXT, self.TITLE_SUBTEXT)
         bar.add('属性', ATTR, v, is_more_utils=True)
 
         overlap = Overlap()
