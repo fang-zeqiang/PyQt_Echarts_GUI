87ed436ed31dd1a0e5347042ed6f567e7032ed17
Hereislittlemushroom <hereislittlemushroom@gmail.com>
2019-06-09 13:37:44 +0800

    itisreallycooltoday

--- test_js_macOS.py
+++ test_js_macOS.py
@@ -11,17 +11,13 @@ from PyQt5.QtWidgets import QDialog, QApplication, QHBoxLayout, QWidget, QGridLa
 from random import randint
 from pyecharts import Bar, Pie, Line, Overlap, Sankey
 from pyecharts_javascripthon.api import TRANSLATOR
-import sys
-
-#TITLE_TEXT = "图表大标题"
-#TITLE_SUBTEXT = "副标题"
-ATTR = ["属性1", "属性2", "属性3", "属性4", "属性5", "属性6"]
-
 
 class Form(QDialog):
 
     TITLE_TEXT = "这是大标题"
     TITLE_SUBTEXT = "这是副标题"
+    ATTR = ["属性1", "属性2", "属性3", "属性4", "属性5", "属性6"]
+
     def __init__(self):
         super(Form, self).__init__()
 
@@ -39,8 +35,8 @@ class Form(QDialog):
         # ATTR1
         #第一个输入属性值的文本框
         self.QLineEdit1=QLineEdit()     # 新建属性输入文本框
-        self.QLineEdit1.setPlaceholderText(ATTR[1-1]) # 默认值是“属性一”
-        self.QLineEdit1.returnPressed.connect(self.reload_canvas)
+        self.QLineEdit1.setPlaceholderText(self.ATTR[1-1]) # 默认值是“属性一”
+        self.QLineEdit1.editingFinished.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit1,1 - 1,0,1,1)# 设置该输入框的排版位置
         self.spinbox1 = QSpinBox()      # 新建SpinBox
         self.spinbox1.setSingleStep(100)# 设置每次调节的单位
@@ -51,7 +47,7 @@ class Form(QDialog):
         self.gl.addWidget(self.spinbox1, 1 - 1, 1, 1, 2)
         # ATTR2
         self.QLineEdit2=QLineEdit()     
-        self.QLineEdit2.setPlaceholderText(ATTR[2-1]) 
+        self.QLineEdit2.setPlaceholderText(self.ATTR[2-1]) 
         self.QLineEdit2.returnPressed.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit2,2 - 1,0,1,1)
         self.spinbox2 = QSpinBox()
@@ -63,7 +59,7 @@ class Form(QDialog):
         self.gl.addWidget(self.spinbox2, 2 - 1, 1, 1, 2)
         # ATTR3
         self.QLineEdit3=QLineEdit()     
-        self.QLineEdit3.setPlaceholderText(ATTR[3-1]) 
+        self.QLineEdit3.setPlaceholderText(self.ATTR[3-1]) 
         self.QLineEdit3.returnPressed.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit3,3 - 1,0,1,1)
         self.spinbox3 = QSpinBox()
@@ -75,7 +71,7 @@ class Form(QDialog):
         self.gl.addWidget(self.spinbox3, 3 - 1, 1, 1, 2)
         # ATTR4
         self.QLineEdit4=QLineEdit()     
-        self.QLineEdit4.setPlaceholderText(ATTR[4-1]) 
+        self.QLineEdit4.setPlaceholderText(self.ATTR[4-1]) 
         self.QLineEdit4.returnPressed.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit4,4 - 1,0,1,1)
         self.spinbox4 = QSpinBox()
@@ -87,7 +83,7 @@ class Form(QDialog):
         self.gl.addWidget(self.spinbox4, 4 - 1, 1, 1, 2)
         # ATTR5
         self.QLineEdit5=QLineEdit()     
-        self.QLineEdit5.setPlaceholderText(ATTR[5-1]) 
+        self.QLineEdit5.setPlaceholderText(self.ATTR[5-1]) 
         self.QLineEdit5.returnPressed.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit5,5 - 1,0,1,1)
         self.spinbox5 = QSpinBox()
@@ -99,7 +95,7 @@ class Form(QDialog):
         self.gl.addWidget(self.spinbox5, 5 - 1, 1, 1, 2)
         # ATTR6
         self.QLineEdit6=QLineEdit()     
-        self.QLineEdit6.setPlaceholderText(ATTR[6-1]) 
+        self.QLineEdit6.setPlaceholderText(self.ATTR[6-1]) 
         self.QLineEdit6.returnPressed.connect(self.reload_canvas)
         self.gl.addWidget(self.QLineEdit6,6 - 1,0,1,1)
         self.spinbox6 = QSpinBox()
@@ -140,16 +136,15 @@ class Form(QDialog):
         self.gl.addWidget(label_kind, 9, 0, 1, 2)
         self.combobox_type = QComboBox()
         self.combobox_type.currentIndexChanged.connect(self.reload_canvas)
-        self.combobox_type.addItems(['饼图', '柱状图', '折线图', '折线\柱状图','桑基图'])
+        self.combobox_type.addItems(['饼状图', '柱状图', '折线图', '折线柱状图','桑基图'])
         self.gl.addWidget(self.combobox_type, 9, 1, 1, 2)
         
         #主题选择按钮
         label_theme = QLabel('主题' + ':')
         self.gl.addWidget(label_theme, 10, 0, 1, 2)
         self.combobox_theme = QComboBox()
+        self.combobox_theme.addItems(['日间模式', '暗黑模式','复古香槟'])
         self.combobox_theme.currentTextChanged.connect(self.change_theme)
-        theme_sets=['日间模式', '暗黑模式','香槟','罗马']
-        self.combobox_theme.addItems(theme_sets)
         self.gl.addWidget(self.combobox_theme, 10, 1, 1, 2)
         # 添加web view
         self.view = QWebEngineView()
@@ -168,9 +163,7 @@ class Form(QDialog):
             theme='dark'
         if self.combobox_theme.currentIndex()==2:
             theme='vintage'
-        if self.combobox_theme.currentIndex()==3:
-            theme='roma'
-        
+
         self.view.page().runJavaScript(
             f'''
                 myChart.dispose();
@@ -194,8 +187,9 @@ class Form(QDialog):
         #重新渲染时，将左侧属性输入框中数据传入图表
         QLineEdit_Array=[self.QLineEdit1,self.QLineEdit2,self.QLineEdit3,self.QLineEdit4,self.QLineEdit5,self.QLineEdit6]
         for i in range(len(QLineEdit_Array)):
-            ATTR[i]= QLineEdit_Array[i].displayText()  
-
+            self.ATTR[i]= QLineEdit_Array[i].displayText()  
+            if self.ATTR[i]=='':
+                self.ATTR[i]='属性'+str(i+1)
         if not self.view:
             return
             # 重载画布
@@ -217,7 +211,7 @@ class Form(QDialog):
             # 初始化echarts
             self.view.page().runJavaScript(
                 '''
-                    var myChart = echarts.init(document.getElementById('container'), 'light', {renderer: 'canvas'});
+                    var myChart = echarts.init(document.getElementById('container'),'light',{renderer: 'canvas'});
                 '''
             )
             self.echarts = True
@@ -249,6 +243,7 @@ class Form(QDialog):
         elif self.combobox_type.currentIndex() == 3:
             # 折线、柱状图
             options = self.create_line_bar(v)
+            # 桑基图
         elif self.combobox_type.currentIndex() == 4:
             options=self.create_sankey(v)
         else:
@@ -257,31 +252,31 @@ class Form(QDialog):
 
     def create_pie(self, v):
         pie = Pie(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        pie.add("属性", ATTR, v, is_label_show=True)
+        pie.add("属性", self.ATTR, v, is_label_show=True)
         snippet = TRANSLATOR.translate(pie.options)
         options = snippet.as_snippet()
         return options
 
     def create_bar(self, v):
         bar = Bar(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        bar.add('属性1', ATTR, v, is_more_utils=True)
-        bar.add('属性2', ATTR, v, is_more_utils=True)
+        bar.add('属性1', self.ATTR, v, is_more_utils=True)
+        bar.add('属性2', self.ATTR, v, is_more_utils=True)
         snippet = TRANSLATOR.translate(bar.options)
         options = snippet.as_snippet()
         return options
 
     def create_line(self, v):
         line = Line(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        line.add("属性", ATTR, v, is_smooth=True, mark_line=["max", "average"])
+        line.add("属性", self.ATTR, v, is_smooth=True, mark_line=["max", "average"])
         snippet = TRANSLATOR.translate(line.options)
         options = snippet.as_snippet()
         return options
 
     def create_line_bar(self, v):
         line = Line(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        line.add("属性", ATTR, v, is_smooth=True, mark_line=["max", "average"])
+        line.add("属性", self.ATTR, v, is_smooth=True, mark_line=["max", "average"])
         bar = Bar(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        bar.add('属性', ATTR, v, is_more_utils=True)
+        bar.add('属性', self.ATTR, v, is_more_utils=True)
         overlap = Overlap()
         overlap.add(line)
         overlap.add(bar)
@@ -291,21 +286,28 @@ class Form(QDialog):
 
     def create_sankey(self, v): #绘制桑基图
         sankey = Sankey(self.TITLE_TEXT, self.TITLE_SUBTEXT)
-        
+        category1,category2,category3,category4,category5,category6=self.ATTR[0],self.ATTR[1],self.ATTR[2],self.ATTR[3],self.ATTR[4],self.ATTR[5]
+
         nodes = [
-            {'name': 'category1'}, {'name': 'category2'}, {'name': 'category3'},
-            {'name': 'category4'}, {'name': 'category5'}, {'name': 'category6'},
+            {'name': category1}, {'name': category2}, {'name': category3},
+            {'name': category4}, {'name': category5}, {'name': category6}
         ]
         links = [
-            {'source': 'category1', 'target': 'category2', 'value': 10},
-            {'source': 'category2', 'target': 'category3', 'value': 15},
-            {'source': 'category3', 'target': 'category4', 'value': 20},
-            {'source': 'category5', 'target': 'category6', 'value': 25}
+            {'source': category1, 'target': category2, 'value': 10},
+            {'source': category2, 'target': category3, 'value': 15},
+            {'source': category3, 'target': category4, 'value': 20},
+            {'source': category5, 'target': category6, 'value': 25}
         ]
-
-        sankey.add( "sankey", nodes,links,line_opacity=0.2,line_curve=0.5,\
-            line_color="source",is_label_show=True,label_pos="right",)
-        
+        sankey.add(
+            "sankey",
+            nodes,
+            links,
+            line_opacity=0.2,
+            line_curve=0.5,
+            line_color="source",
+            is_label_show=True,
+            label_pos="right",
+        )
         snippet = TRANSLATOR.translate(sankey.options)
         options = snippet.as_snippet()
         return options
@@ -313,7 +315,6 @@ class Form(QDialog):
 
 if __name__ == '__main__':
     import sys
-
     app = QApplication(sys.argv)
     app.setStyle('fusion')
     form = Form()
